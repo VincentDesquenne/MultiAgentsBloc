bf2ec869c98c0525a0d6470a6c48b40af64df52b
Vincent Desquenne <vincent.desquenne@hotmail.fr>
2021-10-11 14:41:36 +0200

    Environnement et Main

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static int endIteration = 50;
+
+    public static void main(String[] args) {
+        Bloc blocA = new Bloc();
+        Bloc blocB = new Bloc();
+        Bloc blocC = new Bloc();
+        Bloc blocD = new Bloc();
+        Environnement env = new Environnement();
+        boolean end = false;
+
+        while(end || endIteration == 0){
+
+
+            endIteration--;
+        }
+    }
+}
